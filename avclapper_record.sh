74a780d5a571cba93e220e59c3c8ca349b897fb8
Clifford Wolf <clifford@clifford.at>
2013-10-08 20:49:47 +0200

    Do not set device frame rate in avclapper_record.sh

--- avclapper_record.sh
+++ avclapper_record.sh
@@ -7,7 +7,7 @@ fps=${3:-25}
 id=$( date +"${dev}_%y%m%d%H%M%S" )
 
 convert -size $res xc:none $id.png
-avclapper_avconv -y -f video4linux2 -r $fps -s $res -i /dev/$dev -filter_complex '[0:v]fps=25[vo]' \
+avclapper_avconv -y -f video4linux2 -s $res -i /dev/$dev -filter_complex '[0:v]fps=25[vo]' \
 	-map '[vo]' -c:v libx264 -preset ultrafast $id.mp4 -map 0:v -r 8 -f image2 -update 1 $id.png &
 
 sleep 2
